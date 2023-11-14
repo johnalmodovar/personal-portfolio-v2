df9f2c94f9e08048af54c8d2c4317844facbec24
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-14 00:48:13 -0800

    added margin bottom to title

--- src/components/experience/ExperienceList.jsx
+++ src/components/experience/ExperienceList.jsx
@@ -5,7 +5,7 @@ import jobs from "./experience.json";
 export default function ExperienceList() {
     return (
         <div id="experience" className="max-w-[1040px] m-auto md:pl-20 py-16">
-            <h1 className="text-4xl font-bold text-center text-[#001b5e]">Experience</h1>
+            <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">Experience</h1>
             {jobs.experience.map( (item) => (
                 <ExperienceCard
                     key={item.title}
