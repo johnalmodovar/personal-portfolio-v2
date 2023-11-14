90ae10444e4efd464c36fc70d8053b1596e8ccee
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-14 00:11:53 -0800

    completed ExperienceList component

--- src/components/experience/ExperienceList.jsx
+++ src/components/experience/ExperienceList.jsx
@@ -1,5 +1,19 @@
 import ExperienceCard from "./ExperienceCard";
 
-export default function ExperienceList() {
+const jobs = require("./experience.json");
 
+export default function ExperienceList() {
+    return (
+        <div id="experience" className="max-w-[1040px] m-auto md:pl-20 py-16">
+            <h1 className="text-4xl font-bold text-center text-[#001b5e]">Experience</h1>
+            {jobs.experience.map( (item) => (
+                <ExperienceCard
+                    key={item.title}
+                    year={item.year}
+                    title={item.title}
+                    length={item.length}
+                    details={item.details} />
+            ))}
+        </div>
+    );
 }
