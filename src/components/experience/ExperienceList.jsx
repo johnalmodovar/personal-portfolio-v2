ccd3fdd3cd3671b46707fc4c90b19c1436ac451e
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 01:09:11 -0800

    adjusted padding left/right for mobile, increased padding for lg and up for responsive design

--- src/components/experience/ExperienceList.jsx
+++ src/components/experience/ExperienceList.jsx
@@ -4,7 +4,7 @@ import jobs from "./experience.json";
 
 export default function ExperienceList() {
     return (
-        <div id="experience" className="max-w-[1040px] m-auto md:pl-20 py-16">
+        <div id="experience" className="max-w-[1040px] m-auto px-4 md:pl-20 xl:pl-0 2xl:pl-0 py-16">
             <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">Experience</h1>
             {jobs.experience.map( (item) => (
                 <ExperienceCard
