f26c81fa467c2daccb40b40358cf7aa3f468215c
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 01:29:23 -0800

    readjusted padding

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
     return(
     <div id="about" className="max-w-[1040px] m-auto px-4 py-16">
         <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">About</h1>
-        <p className="my-2 text-base font-normal text-stone-500 md:pl-20 xl:pl-0 2xl:pl-0">
+        <p className="my-2 text-base font-normal text-stone-500 md:pl-16 xl:pl-0 2xl:pl-0">
             Before I became a software engineer, I pursued a career in Marketing - until a close friend of mine encouraged me to making a
             switch to the tech industry. He believed I had the skills needed to be successful in this field, and I would enjoy it because I
             have an inherent passion for tech and computers. Having built PCs for myself and others, I started to believe in his words and decided
@@ -11,13 +11,13 @@ export default function About() {
             I immersed myself into the world of coding, the more I realized this is truly the career I wanted to pursue.
         </p>
         <br />
-        <p className="my-2 text-base font-normal text-stone-500 md:pl-20 xl:pl-0 2xl:pl-0">
+        <p className="my-2 text-base font-normal text-stone-500 md:pl-16 xl:pl-0 2xl:pl-0">
             Fast forward to the present, I've graduated from Rithm School's full-stack engineering program, and I've transformed from a marketing analyst
             into a software engineer. During this time, I've learned that I love to build applications that everybody admires, and had the privilege to work
             and learn from amazing engineers, and have a taste of what it's like to build and contribute to innovative applications.
         </p>
         <br />
-        <p className="my-2 text-base font-normal text-stone-500 md:pl-20 xl:pl-0 2xl:pl-0">
+        <p className="my-2 text-base font-normal text-stone-500 md:pl-16 xl:pl-0 2xl:pl-0">
             Currently, I am looking for job opportunities and looking forward to contribute to the tech industry - building innovative applications, websites,
             and other cutting-edge projects.
         </p>
--- src/components/experience/ExperienceList.jsx
+++ src/components/experience/ExperienceList.jsx
@@ -6,7 +6,7 @@ export default function ExperienceList() {
     return (
         <div id="experience" className="max-w-[1040px] m-auto px-4 py-16">
             <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">Experience</h1>
-            <div className="md:pl-20 xl:pl-0 2xl:pl-0">
+            <div className="md:pl-16 xl:pl-0 2xl:pl-0">
                 {jobs.experience.map( (item) => (
                     <ExperienceCard
                         key={item.title}
