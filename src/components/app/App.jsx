0c1bc8f93324d9c3c029d39bf3de3881274275f5
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 01:08:33 -0800

    added About component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from '../navbar/Navbar'
 import Main from '../main/Main'
+import About from '../about/About'
 import ExperienceList from '../experience/ExperienceList'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Main />
+      <About />
       <ExperienceList />
     </div>
   )
