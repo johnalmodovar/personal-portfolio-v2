8540a6ebc67586497fbbc4a52b8e82be6768a953
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 02:27:32 -0800

    finished component

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -1,4 +1,9 @@
 
-export default function SkillCard() {
-
+export default function SkillCard({ src, name }) {
+    return (
+        <div className="flex flex-col justify-center relative py-4 items-center">
+            <img src={src} className="w-[80px]" />
+            <span className="my-2 text-base text-center font-normal text-stone-500">{name}</span>
+        </div>
+    );
 }
--- src/components/skills/SkillsList.jsx
+++ src/components/skills/SkillsList.jsx
@@ -1,5 +1,20 @@
 import SkillCard from "./SkillCard";
 
-export default function SkillsList() {
+import skills from "./skills.json";
 
+export default function SkillsList() {
+    return (
+        <div id="skills" className="max-w-[1040px] m-auto px-4 py-16">
+            <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">Skills</h1>
+            <div className="grid grid-cols-4 gap-4 relative pt-4 md:pl-8 xl:pl-0 2xl:pl-0">
+                {skills.src.map( (src) => (
+                    <SkillCard
+                        key={src}
+                        src={src.link}
+                        name={src.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
