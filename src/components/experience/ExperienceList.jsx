af9bc731d62e19d6d0802a41dd4f82a28d71216b
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 01:26:52 -0800

    fixed padding for header so it stays centered through all breakpoints, and fixed padding for body to make room for side navbar

--- src/components/experience/ExperienceList.jsx
+++ src/components/experience/ExperienceList.jsx
@@ -4,16 +4,18 @@ import jobs from "./experience.json";
 
 export default function ExperienceList() {
     return (
-        <div id="experience" className="max-w-[1040px] m-auto px-4 md:pl-20 xl:pl-0 2xl:pl-0 py-16">
+        <div id="experience" className="max-w-[1040px] m-auto px-4 py-16">
             <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">Experience</h1>
-            {jobs.experience.map( (item) => (
-                <ExperienceCard
-                    key={item.title}
-                    year={item.year}
-                    title={item.title}
-                    length={item.length}
-                    details={item.details} />
-            ))}
+            <div className="md:pl-20 xl:pl-0 2xl:pl-0">
+                {jobs.experience.map( (item) => (
+                    <ExperienceCard
+                        key={item.title}
+                        year={item.year}
+                        title={item.title}
+                        length={item.length}
+                        details={item.details} />
+                ))}
+            </div>
         </div>
     );
 }
