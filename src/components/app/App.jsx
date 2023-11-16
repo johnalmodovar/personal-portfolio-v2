a7fc7e36b58876940770a66ac4291dda1dedc21a
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 22:46:09 -0800

    added ProjectsList component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import Navbar from '../navbar/Navbar'
 import Main from '../main/Main'
 import About from '../about/About'
 import SkillsList from '../skills/SkillsList'
+import ProjectsList from '../projects/ProjectsList'
 import ExperienceList from '../experience/ExperienceList'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Main />
       <About />
       <SkillsList />
+      <ProjectsList />
       <ExperienceList />
     </div>
   )
