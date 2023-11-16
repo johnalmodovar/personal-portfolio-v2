e4a8b53e08c8de8eb0b67b7d90cbe0c751e8da72
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-15 23:25:24 -0800

    made component able to read props coming from json

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,4 +1,24 @@
 
-export default function ProjectCard() {
 
+export default function ProjectCard({ title, img, demo, github }) {
+    return (
+        <div className="relative flex items-center justify-center h-auto w-auto shadow-lg shadow-gray-400 rounded-xl group">
+            <img src={img} alt={title} className="rounded-xl group-hover:opacity-10" />
+            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                <h3 className="text-3xl md:text-2xl font-bold text-[#001b5e] tracking-wider text-center mb-8">{title}</h3>
+                <div className="flex justify-between">
+                    <div className="flex items-center rounded-lg h-10 m-4 bg-gray-100 shadow-md shadow-gray-400 hover:scale-110 ease-in duration-200">
+                        <a href={demo} target="_blank">
+                            <p className="text-center p-3">Demo</p>
+                        </a>
+                    </div>
+                    <div className="flex items-center rounded-lg h-10 m-4 bg-gray-100 shadow-md shadow-gray-400 hover:scale-110 ease-in duration-200">
+                        <a href={github} target="_blank">
+                            <p className="text-center p-3">GitHub</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -3,13 +3,23 @@ import ProjectCard from "./ProjectCard";
 import joblyImg from "../../assets/projects/jobly.png";
 import sharebnbImg from "../../assets/projects/sharebnb.png";
 
+import projects from "./projects.json";
+
 export default function ProjectsList() {
     return (
         <div id="projects" className="max-w-[1040px] m-auto px-4 py-16">
             <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">Projects</h1>
             <div className="grid sm:grid-cols-2 gap-12 relative pt-4 md:pl-16 xl:pl-0 2xl:pl-0">
-                <ProjectCard img={joblyImg} title="Jobly" />
-                <ProjectCard img={sharebnbImg} title="ShareBnB" />
+                <ProjectCard
+                    title={projects.jobly.title}
+                    img={joblyImg}
+                    demo=""
+                    github={projects.jobly.github} />
+                <ProjectCard
+                    title={projects.sharebnb.title}
+                    img={sharebnbImg}
+                    demo=""
+                    github={projects.sharebnb.github} />
             </div>
         </div>
     )
