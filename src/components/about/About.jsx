d4225f2af45ffa101bfaa0ec975de8f659f5c0fe
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-03-04 15:39:38 -0800

    overhauled about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,22 +4,16 @@ export default function About() {
     <div id="about" className="max-w-[1040px] m-auto px-4 py-16">
         <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-4">About</h1>
         <p className="my-2 text-base font-normal text-stone-500 md:pl-16 xl:pl-0 2xl:pl-0">
-            Before I became a software engineer, I pursued a career in Marketing - until a close friend of mine encouraged me to making a
-            switch to the tech industry. He believed I had the skills needed to be successful in this field, and I would enjoy it because I
-            have an inherent passion for tech and computers. Having built PCs for myself and others, I started to believe in his words and decided
-            to apply what I know about PC building to software development. I began with learning JavaScript and the fundamentals of coding, and the more
-            I immersed myself into the world of coding, the more I realized this is truly the career I wanted to pursue.
+        I’m a Software Engineer that specializes in front-end development that loves to make innovative apps that make for a great user experience! In the past,
+        I’ve held multiple managerial positions in the food and sales industry.
         </p>
         <br />
         <p className="my-2 text-base font-normal text-stone-500 md:pl-16 xl:pl-0 2xl:pl-0">
-            Fast forward to the present, I've graduated from Rithm School's full-stack engineering program, and I've transformed from a marketing analyst
-            into a software engineer. During this time, I've learned that I love to build applications that everybody admires, and had the privilege to work
-            and learn from amazing engineers, and have a taste of what it's like to build and contribute to innovative applications.
+        With a marketing background, I’ve developed a product and design-minded perspective as well as strong interpersonal skills from my previous experiences.
         </p>
         <br />
         <p className="my-2 text-base font-normal text-stone-500 md:pl-16 xl:pl-0 2xl:pl-0">
-            Currently, I am looking for job opportunities and looking forward to contribute to the tech industry - building innovative applications, websites,
-            and other cutting-edge projects.
+            In my spare time, I love watching anime, playing video games, and hanging out with my cat Yuna!
         </p>
     </div>
     );
